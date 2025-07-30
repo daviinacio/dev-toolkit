21b4099033e0dab4de891ba0f467c5fbd7a76b3a
Davi Inácio <aazz6850@gmail.com>
2025-07-30 11:37:42 -0300

    PWA enabled

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,12 +1,27 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
- 
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Dev Toolkit by {daviinacio}",
+        short_name: "Dev Toolkit",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest}"],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  }
-})
+  },
+});
