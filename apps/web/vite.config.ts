8ea37a412f58f6468062832a10b2ea313d10ca35
Davi Inácio <aazz6850@gmail.com>
2025-07-31 18:13:03 -0300

    PWA autoUpdate

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,12 +1,13 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-import { defineConfig } from "vite";
+import { defineConfig, rollupVersion } from "vite";
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      registerType: "autoUpdate",
       manifest: {
         name: "Dev Toolkit by {daviinacio}",
         short_name: "Dev Toolkit",
@@ -16,6 +17,8 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest}"],
+        cleanupOutdatedCaches: false,
+        sourcemap: true,
       },
     }),
   ],
